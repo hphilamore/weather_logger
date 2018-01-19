9cc14d4d87c7a32b7863d6a5735e045a94cfd495
hphilamore <hemmaphilamore@gmail.com>
2018-01-19 14:27:50 +0900

    tidy up

--- basic_datalog/basic_datalog.ino
+++ basic_datalog/basic_datalog.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <SD.h>
+#include <Wire.h>
 
 // Set the pins used
 #define cardSelect 4
@@ -25,8 +26,7 @@ void error(uint8_t errno) {
   }
 }
 
-// This line is not needed if you have Adafruit SAMD board package 1.6.2+
-// #define Serial SerialUSB
+
 
 void setup() {
   // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
@@ -34,43 +34,32 @@ void setup() {
   Serial.begin(115200);
   Serial.println("\r\nAnalog logger test");
   pinMode(13, OUTPUT);
+
   
   // see if the card is present and can be initialized:
   if (!SD.begin(cardSelect)) {
-  Serial.println("Card init. failed!");
-  error(2);
+    Serial.println("Card init. failed!");
+    error(2);
   }
-  
-//  // create text array with desired number of characters
-//  char filename[15];
-  
+
+    
   // assign the desired file name to the variable filename 
   strcpy(filename, "ANALOG00.TXT");
+
 
   // loop through 100 possible filenames...
   for (uint8_t i = 0; i < 100; i++) {
     filename[6] = '0' + i/10;
     filename[7] = '0' + i%10;
+
     
     // ...stop when an unused filename is reached
     if (! SD.exists(filename)) {
+      Serial.print("Writing to ");
+      Serial.println(filename);
       break;
     }
   }
-
-//  // open the file to write data
-//  logfile = SD.open(filename, FILE_WRITE);
-//  if( ! logfile ) {
-//    Serial.print("Couldnt create ");
-//    Serial.println(filename);
-//    error(3);
-//  }
-//  
-//  Serial.print("Writing to ");
-//  Serial.println(filename);
-//  pinMode(13, OUTPUT);
-//  pinMode(8, OUTPUT);
-//  Serial.println("Ready!");
 }
 
 
@@ -79,30 +68,37 @@ void loop() {
 
   // open the file to write data
   logfile = SD.open(filename, FILE_WRITE);
+
+  
+  // if there is no SD card, print error message
   if( ! logfile ) {
-    Serial.print("Couldnt create ");
+    Serial.print("Couldnt create/open file: ");
     Serial.println(filename);
     error(3);
   }
-  
-  Serial.print("Writing to ");
-  Serial.println(filename);
+
+
+  // on baord LED pins
   pinMode(13, OUTPUT);
   pinMode(8, OUTPUT);
-  Serial.println("Ready!");
+
   
   // LED on
   digitalWrite(8, HIGH);
+
 
   // write reading to file on SD card and serial monitor
   logfile.print("A0 = "); logfile.println(analogRead(0));
   Serial.print("A0 = "); Serial.println(analogRead(0));
 
+
   // LED off
   digitalWrite(8, LOW);
 
+
   // 100 ms delay between readings
   delay(1000);
+
 
   // close the file ensuring that written data is physically saved to the SD card
   logfile.close();
